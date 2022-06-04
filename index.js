9a96e324bae27bde2b7076d0f950cac0c30c3dcd
Abhishek Verma <abhishekverma998@gmail.com>
2022-06-04 12:25:21 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Vue version 2 Syntax
+var app = new Vue({
+  el: "#app", // Refers to the element having app as id
+  data: {
+    Count: 0,
+  },
+})
+
+// Vue version 3 Syntax
+// import { createApp } from "vue"
+
+// createApp({
+//   data() {
+//     return {
+//       Count: 0,
+//     }
+//   },
+// }).mount("#app")
